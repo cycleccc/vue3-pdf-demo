5ebd5a048fad4372f7c1c18fe8926ffdb9b1af5f
yaolongfei <2991205548@qq.com>
2024-12-04 12:41:47 +0800

    chore: add github page deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue';
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [vue()],
+    base: '/vue3-pdf-demo/',
     build: {
         target: 'esnext', // 确保支持私有字段
     },
